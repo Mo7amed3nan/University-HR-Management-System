2d193d89dc876fdc0f7bffec037a632a9aef06d9
Youssef <themasterofsuspence3@gmail.com>
2025-12-01 19:26:28 +0200

    ignore

--- Mile Stone 3/Uni_HR_Management_System/obj/Debug/net8.0/Uni_HR_Management_System.AssemblyInfo.cs
+++ Mile Stone 3/Uni_HR_Management_System/obj/Debug/net8.0/Uni_HR_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uni_HR_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842b95416abf0600ecbb7faca109b2fb023441aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375a912b3832f3fd06109c660427612a12d3fe88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uni_HR_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uni_HR_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
